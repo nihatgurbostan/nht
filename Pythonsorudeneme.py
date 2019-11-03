a1b859bc2ad0bc60bd6cce6c3241a9ac0c718941
nihatgurbostan <57326527+nihatgurbostan@users.noreply.github.com>
2019-11-03 20:38:26 +0300

    Update Pythonsorudeneme.py

--- Pythonsorudeneme.py
+++ Pythonsorudeneme.py
@@ -1,5 +1,6 @@
-def check_for_number(value):
 
+
+def check_for_number(value):
 
     if sum([int(i) for i in str(value)]) == sum([int(i) for i in str(2*value)]) == sum([int(i) for i in str(3*value)]) == sum([int(i) for i in str(4*value)]) == sum([int(i) for i in str(5*value)]) == sum([int(i) for i in str(6*value)]):
         return True
@@ -7,11 +8,13 @@ def check_for_number(value):
         return False
 
 
-
-
 results_list = list()
-for i in range(1000000):
+
+for i in range(1,1000000):
+
     if check_for_number(i):
         results_list.append(i)
+
 print(results_list)
+
 
